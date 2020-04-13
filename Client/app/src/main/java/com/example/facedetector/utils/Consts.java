2cfd9c0180d0bcd007a40ddaa0abc356a96fde71
Maxim190 <vbrc1212@yandex.ru>
2020-04-14 01:07:06 +0300

    Add consts file

---
+++ Client/app/src/main/java/com/example/facedetector/utils/Consts.java
@@ -0,0 +1,20 @@
+package com.example.facedetector.utils;
+
+public class Consts {
+    public final static String MSG_TYPE_RECOGNIZE = "RECOGNIZE";
+    public final static String MSG_TYPE_ADD = "ADD";
+    public final static String MSG_TYPE_EDIT = "EDIT";
+    public final static String MSG_TYPE_DELETE = "DELETE";
+    public final static String MSG_TYPE_CHECK = "CHECK";
+
+    public final static String DATA_TYPE_PHOTO = "PHOTO";
+    public final static String DATA_TYPE_ID = "id";
+    public final static String DATA_TYPE_NAME = "name";
+    public final static String DATA_TYPE_LAST_NAME = "last_name";
+    public final static String DATA_TYPE_BIRTH = "birth";
+    public final static String DATA_TYPE_DEPARTMENT = "department_id";
+    public final static String DATA_TYPE_CODE = "CODE";
+
+    public final static String CODE_SUCCESS = "1";
+    public final static String CODE_ERROR = "2";
+}
