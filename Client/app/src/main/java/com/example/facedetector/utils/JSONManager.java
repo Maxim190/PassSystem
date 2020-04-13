3c9d15baaf6f36c480c140306e7b97cbabedb9fe
Maxim190 <vbrc1212@yandex.ru>
2020-04-14 01:08:37 +0300

    add class for working with JSON data

---
+++ Client/app/src/main/java/com/example/facedetector/utils/JSONManager.java
@@ -0,0 +1,50 @@
+package com.example.facedetector.utils;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+
+public class JSONManager {
+
+    public static Map<String, String> parse(String str) {
+        if (str == null || str.isEmpty()) {
+            return null;
+        }
+        //Log.e("PassSystem", "JSON_MANAGER PARSE: " + str);
+        String precessedStr = str.replaceAll("[\\[\\]{}\\s]", "");
+        String[] pairsArray = precessedStr.split(",");
+
+        Map<String, String> result = new LinkedHashMap<>();
+        for (String item: pairsArray) {
+            String[] pair = item.split(":");
+            if (pair.length != 2) {
+                continue;
+            }
+            String key = pair[0].replaceAll("[\"']", "");
+            String value = pair[1].replaceAll("[\"']", "");
+            result.put(key, value);
+        }
+        //result.forEach((key, value)->Log.e("PassSystem", "PARSED: " + key + " " + value));
+        return result;
+    }
+
+    public static String dump(Map<String, String> data) {
+        StringBuilder result = new StringBuilder("{");
+        String firstKey = data.keySet().iterator().next();
+        for (String key: data.keySet()) {
+            if (!key.equals(firstKey)) {
+                result.append(',');
+            }
+            result.append('"')
+                .append(key)
+                .append('"')
+                .append(':')
+                .append('"')
+                .append(data.get(key))
+                .append('"');
+        }
+        result.append('}');
+        return result.toString();
+    }
+}
