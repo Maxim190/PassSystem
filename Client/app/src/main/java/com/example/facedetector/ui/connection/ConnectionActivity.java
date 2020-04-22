0868204029cf3839a4c2cb983543cd60ec258556
Maxim190 <vbrc1212@yandex.ru>
2020-04-22 12:29:20 +0300

    redesign conection activity

--- Client/app/src/main/java/com/example/facedetector/ui/connection/ConnectionActivity.java
+++ Client/app/src/main/java/com/example/facedetector/ui/connection/ConnectionActivity.java
@@ -2,6 +2,7 @@ package com.example.facedetector.ui.connection;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -10,34 +11,55 @@ import androidx.annotation.Nullable;
 
 import com.example.facedetector.R;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class ConnectionActivity extends Activity implements ConnectionViewContract.View {
 
     private ConnectionViewContract.Presenter presenter;
-    private EditText ipTextView;
+    private EditText ipField;
     private TextView log;
+    private Button connectBtn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.connection_fragment);
 
-        ipTextView = findViewById(R.id.editText_ip);
         log = findViewById(R.id.textView_log);
+        ipField = findViewById(R.id.editText_ip);
 
-        Button connectBtn = findViewById(R.id.button_connect);
-        connectBtn.setOnClickListener(i->presenter.connect());
+        connectBtn = findViewById(R.id.button_connect);
+        setEnableIfLongClick();
 
         presenter = new ConnectionPresenter(this);
     }
 
+    private void setEnableIfLongClick() {
+        AtomicBoolean isEnabled = new AtomicBoolean(false);
+        connectBtn.setOnClickListener(v-> {
+            if (isEnabled.get()) {
+                connectBtnClicked(connectBtn);
+            }
+        });
+        connectBtn.setOnLongClickListener(v-> {
+            isEnabled.set(true);
+            v.setBackground(getResources().getDrawable(R.drawable.button_pink_oval, null));
+            return true;
+        });
+    }
+
+    public void connectBtnClicked(View view) {
+        presenter.connect();
+    }
+
     @Override
     public String getIp() {
-        return ipTextView.getText().toString();
+        return ipField.getText().toString();
     }
 
     @Override
     public void displayLog(String msg) {
-        runOnUiThread(()->log.setText(msg));
+        runOnUiThread(()->log.append("\n" + msg));
     }
 
     @Override
